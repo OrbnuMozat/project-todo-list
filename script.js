057de3bde65c0cfddf8200179617fd907278c6fb
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-04-28 18:56:08 -0300

    travado no 7

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const listaTarefas = document.getElementById('lista-tarefas');
 const criarTarefa = document.getElementById('criar-tarefa');
 let tarefa;
 
-function capTexto(event) {
+function capTexto() {
   tarefa = textoTarefa.value;
 }
 
@@ -11,9 +11,23 @@ textoTarefa.addEventListener('input', capTexto);
 
 function addTarefa() {
   const createLi = document.createElement('li');
-  createLi.innerText = tarefa;
+  createLi.innerHTML = tarefa;
+  createLi.className = 'tarefas';
   listaTarefas.appendChild(createLi);
   textoTarefa.value = '';
 }
 
 criarTarefa.addEventListener('click', addTarefa);
+
+function listSelect(event) {
+  // const list = document.querySelectorAll('.tarefas');
+  // for (let i = 0; i < list.length; i += 1) {
+  //   // list.className.remove('selected');
+  // }
+  event.target.classList.add('selected');
+}
+const liTarefas = document.querySelectorAll('.tarefas');
+
+liTarefas.forEach((item) => {
+  item.addEventListener('click', listSelect);
+});
