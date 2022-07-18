3526fefb19ad37b5cbbb0ca2aa9c4557db01f2f6
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-07-17 22:16:24 -0300

    preparando para pages

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const criarTarefa = document.getElementById('criar-tarefa');
 let tarefa;
 const btnLimpa = document.getElementById('apaga-tudo');
 const btkRemoveOk = document.getElementById('remover-finalizados');
+const btnSave = document.getElementById('salvar-tarefas');
 
 function capTexto() {
   tarefa = textoTarefa.value;
@@ -11,15 +12,15 @@ function capTexto() {
 
 textoTarefa.addEventListener('input', capTexto);
 
-function addTarefa() {
+function addTarefa(newTarefa) {
   const createLi = document.createElement('li');
-  createLi.innerText = tarefa;
+  createLi.innerText = newTarefa;
   createLi.className = 'tarefas';
   listaTarefas.appendChild(createLi);
   textoTarefa.value = '';
 }
 
-criarTarefa.addEventListener('click', addTarefa);
+criarTarefa.addEventListener('click', () => addTarefa(tarefa));
 
 function listSelect(event) {
   const liTarefas = document.querySelectorAll('li.tarefas');
@@ -47,14 +48,40 @@ function clear() {
   while (listaTarefas.hasChildNodes()) {
     listaTarefas.removeChild(listaTarefas.firstChild);
   }
+  localStorage.clear();
 }
 
 btnLimpa.addEventListener('click', clear);
 
+function removeCompletedInStorage(task) {
+  const saved = JSON.parse(localStorage.getItem('savedList'));
+  const newSaved = saved.filter((item) => item !== task);
+  localStorage.setItem('savedList', JSON.stringify(newSaved));
+}
+
 function removeOk() {
   const liCompleted = document.querySelectorAll('li.completed');
   for (let i = 0; i < liCompleted.length; i += 1) {
+    removeCompletedInStorage(liCompleted[i].innerHTML);
     listaTarefas.removeChild(liCompleted[i]);
   }
 }
 btkRemoveOk.addEventListener('click', removeOk);
+
+function saveButton() {
+  const tarefas = document.querySelectorAll('li.tarefas');
+  const tarefasToSave = Object.values(tarefas).map((item) => item.innerHTML);
+  localStorage.setItem('savedList', JSON.stringify(tarefasToSave));
+}
+btnSave.addEventListener('click', saveButton);
+
+function recoverSaved() {
+  const saved = JSON.parse(localStorage.getItem('savedList'));
+  if (saved) {
+    return saved.map((item) => addTarefa(item));
+  }
+}
+
+window.onload = () => {
+  recoverSaved();
+};
